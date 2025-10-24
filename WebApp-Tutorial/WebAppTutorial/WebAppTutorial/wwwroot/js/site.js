2331fe7f74d73500b0e6c8b9034b16c099b158f6
Garrett <wicketspaw@gmail.com>
2025-10-24 09:50:12 -0700

    for Vs submission assignment

---
+++ WebApp-Tutorial/WebAppTutorial/WebAppTutorial/wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//Function to be called onmouseover event and onmouseout event to change the color of Hello World text
+function newColor() {
+    document.getElementById("hello_world").style.color = "blue";
+}
+
+function oldColor() {
+    document.getElementById("hello_world").style.color = "darkolivegreen";
+}
+
+var id = null;
+function myMove() {
+    var elem = document.getElementById("myAnimate");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
+}
